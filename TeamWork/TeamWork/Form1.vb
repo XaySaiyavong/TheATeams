729f923c213286a86134dddf8b3feda0cc887d82
xengpao <xengpao1996@gmail.com>
2016-05-07 15:27:35 +0700

    2nd Commit

--- TeamWork/TeamWork/Form1.vb
+++ TeamWork/TeamWork/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MessageBox.Show("Welcome to Our Team")
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("Hello")
+    End Sub
 End Class
